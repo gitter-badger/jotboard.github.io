033de2f7837672fb17783b9bff2e1c416ee7b1d3
Chris Lowles <gamer381@gmail.com>
2014-06-06 17:46:39 +1200

    Update beta.js

--- files/beta.js
+++ files/beta.js
@@ -16,3 +16,19 @@ $.ajax({
 }).error(function(e) {
         console.warn("Gist save error", e);
 });
+
+jQuery.fn.tn = function(tn, tn1, tn2, tn3) {
+        // Upcoming Universal function 
+        if (tn == "Attr") {
+                return this.each(function() {
+                        if ($(this).attr(tn1) == tn2) $(this).attr(tn1, tn3);
+                        else $(this).attr(tn1, tn2);
+                });
+        }
+        if (tn == "HTML") {
+                return this.each(function() {
+                        if ($(this).html() == tn1) $(this).html(tn2);
+                        else $(this).html(tn1);
+                });
+        }
+};
