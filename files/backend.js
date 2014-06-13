eb2519dee916c993241aed5cf6b2605689d0f518
Chris Lowles <gamer381@gmail.com>
2014-06-14 02:58:44 +1200

    Update backend.js

--- files/backend.js
+++ files/backend.js
@@ -2,10 +2,10 @@ $(window).load(function() {
         $(function() {
                 var protocolCheck = function() {
                                 if (window.location.protocol("http:")) {
-                                        return "http:";
+                                        return protocol = "https:";
                                 }
                                 if (window.location.protocol("https:")) {
-                                        return "https:";
+                                        return protocol = "https:";
                                 }
                         };
                 protocolCheck();
@@ -94,13 +94,13 @@ $(window).load(function() {
         });
 });
 
-$(window).tn("tnLoad", protocolCheck + "//cdnjs.cloudflare.com/ajax/libs/store.js/1.3.14/store.min.js", function() {
+$(window).tn("tnLoad", protocol + "//cdnjs.cloudflare.com/ajax/libs/store.js/1.3.14/store.min.js", function() {
         console.info("Store.js loaded.");
         var form = document.querySelectorAll("#form");
         var namespace = document.querySelectorAll("#namespace");
 });
 
-$(window).tn("tnLoad", protocolCheck + "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/moment.min.js", function() {
+$(window).tn("tnLoad", protocol + "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/moment.min.js", function() {
         console.log("Moment.JS Loaded");
         var UT = function() {
                         // Making it work
@@ -117,7 +117,7 @@ $(window).tn("tnLoad", protocolCheck + "//cdnjs.cloudflare.com/ajax/libs/moment.
         setInterval(UT, 1);
 });
 
-$(window).tn("tnLoad", "//togetherjs.com/togetherjs-min.js", function() {
+$(window).tn("tnLoad", protocol + "//togetherjs.com/togetherjs-min.js", function() {
         // Groupies
         TogetherJSConfig_siteName = "Textnet";
         TogetherJSConfig_toolName = "Groupies";
@@ -129,16 +129,16 @@ $(window).tn("tnLoad", "//togetherjs.com/togetherjs-min.js", function() {
 });
 
 $(function() {
-        $(window).tn("tnLoad", protocolCheck + "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js", function() {
+        $(window).tn("tnLoad", protocol + "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js", function() {
                 console.info("Bootstrap (JS) Loaded");
         });
-        $(window).tn("tnLoad", protocolCheck + "//cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js", function() {
+        $(window).tn("tnLoad", protocol + "//cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js", function() {
                 console.info("Prefixfree Loaded");
         });
-        $(window).tn("tnLoad", protocolCheck + "//cdnjs.cloudflare.com/ajax/libs/bootstrap-growl/1.0.0/jquery.bootstrap-growl.min.js", function() {
+        $(window).tn("tnLoad", protocol + "//cdnjs.cloudflare.com/ajax/libs/bootstrap-growl/1.0.0/jquery.bootstrap-growl.min.js", function() {
                 console.info("Bootstrap Growl Loaded");
         });
-        $(window).tn("tnLoad", protocolCheck + "//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.2.0/bootbox.min.js", function() {
+        $(window).tn("tnLoad", protocol + "//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.2.0/bootbox.min.js", function() {
                 console.log("Bootbox Loaded");
         });
 });
