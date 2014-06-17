54c7cf7b419c6ff170052d3652d18fbede351ae3
Chris Lowles <gamer381@gmail.com>
2014-06-17 12:42:32 +1000

    Update beta.js

--- files/beta.js
+++ files/beta.js
@@ -1,3 +1,23 @@
+var scripts;
+var stylesheets;
+
+scripts = ["1.js", "2.js"];
+stylesheets = ["1.css", "2.css"];
+
+scripts.forEach(function(src) {
+        var script = document.createElement('script');
+        script.src = src;
+        script.async = false;
+        document.head.appendChild(script);
+});
+
+stylesheets.forEach(function(src) {
+        var script = document.createElement('script');
+        script.src = src;
+        script.async = false;
+        document.head.appendChild(script);
+});
+
 $.ajax({
         url: "https://api.github.com/gists",
         type: "POST",
@@ -15,27 +35,4 @@ $.ajax({
         console.info("Gist Save Success", "Tree: " + e);
 }).error(function(e) {
         console.error("Gist Save Error", "Tree: " + e);
-});
-
-var scripts;
-scripts = ["1.js", "2.js"];
-
-scripts.forEach(function(src) {
-        var script = document.createElement('script');
-        script.src = src;
-        script.async = false;
-        document.head.appendChild(script);
-});
-
-var elementMake1 = document.createElement("div");
-var elementClass = elementMake1.className("inner-data");
-
-var cogs = {
-        items: ['item 1', 'item 2', 'item 3']
-};
-
-$(document).load(function() {
-        $(cogs).each(function(index, item) {
-                $("<div>").appendTo("body").append($(elementMake1).text(item));
-        });
 });
