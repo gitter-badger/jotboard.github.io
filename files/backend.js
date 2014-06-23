244c26771bbcee0377bbdd418cec7b0339d3e5e9
Chris Lowles (Sociao) <sociao@users.noreply.github.com>
2014-06-23 14:09:23 +1200

    Update backend.js

--- files/backend.js
+++ files/backend.js
@@ -21,9 +21,6 @@ $(window).load(function() {
                         if (hashline("yt")) {
                                 $(window).tn("execute", "youtube");
                         }
-                        if (hashline("yt") && hashline("blackpanthaa")) {
-                                $(window).tn("execute", "youtube-s-blackpanthaa");
-                        }
                         if (hashline("new") || hashline("n")) {
                                 event();
                         }
@@ -32,10 +29,6 @@ $(window).load(function() {
                 }
         });
         $(function() {
-                // Checking if there is any data in the Main Textnet.
-                if (window.localStorage["_-Main"]) {
-                        form.value = store.get("_-Main");
-                }
                 bootbox.setDefaults({
                         backdrop: false,
                         animate: true
@@ -51,97 +44,106 @@ $(window).load(function() {
 $(function() {
         $(".tn-event").click(function() { event(); });
         var event = function() {
-                        if (hashline("event")) {
-                                if (window.location.protocol == "https:") {
-                                        event();
-                                        document.title = "Event Hub / Textnet";
-                                }
-                                if (window.location.protocol == "http:") {
-                                        event();
-                                        document.title = "(Unsecure) Event Hub / Textnet";
-                                }
+                if (hashline("event")) {
+                        if (window.location.protocol == "https:") {
+                                event();
+                                document.title = "Event Hub / Textnet";
                         }
-                        bootbox.dialog({
-                                message: "<iframe class='yt-player' width='100%' height='310' src='https://www.youtube.com/embed/?listType=playlist&list=PLXJjNJMpJQKqbpmhNOJNETiMbfZpLjIVb&autoplay=1&fs=1&loop=1&showinfo=0&autohide=1&theme=light' frameborder='0' allowfullscreen></iframe>",
-                                title: "E3",
-                                buttons: {
-                                        facebook: {
-                                                label: "Facebook", className: "btn-primary",
-                                                callback: function() {
-                                                        window.open("https://www.facebook.com/hashtag/E3", "_blank");
-                                                        return false;
-                                                }
-                                        },
-                                        twitter: {
-                                                label: "Twitter", className: "btn-info",
-                                                callback: function() {
-                                                        window.open("https://twitter.com/search?q=%23E3", "_blank");
-                                                        return false;
-                                                }
-                                        },
-                                        twitch: {
-                                                label: "Twitch", className: "btn-link",
-                                                callback: function() {
-                                                        window.open("http://www.twitch.tv/event/e3", "_blank");
-                                                        return false;
-                                                }
-                                        },
-                                        official: {
-                                                label: "Website", className: "btn-link",
-                                                callback: function() {
-                                                        window.open("http://www.e3expo.com/", "_blank");
-                                                        return false;
-                                                }
-                                        },
-                                        youtube: {
-                                                label: "YouTube", className: "btn-danger",
-                                                callback: function() {
-                                                        window.open("https://www.youtube.com/results?search_query=E3", "_blank");
-                                                        return false;
-                                                }
+                        if (window.location.protocol == "http:") {
+                                event();
+                                document.title = "(Unsecure) Event Hub / Textnet";
+                        }
+                }
+                bootbox.dialog({
+                        message: "<iframe class='yt-player' width='100%' height='310' src='https://www.youtube.com/embed/?listType=playlist&list=PLXJjNJMpJQKqbpmhNOJNETiMbfZpLjIVb&autoplay=1&fs=1&loop=1&showinfo=0&autohide=1&theme=light' frameborder='0' allowfullscreen></iframe>",
+                        title: "E3",
+                        buttons: {
+                                facebook: {
+                                        label: "Facebook", className: "btn-primary",
+                                        callback: function() {
+                                                window.open("https://www.facebook.com/hashtag/E3", "_blank");
+                                                return false;
+                                        }
+                                },
+                                twitter: {
+                                        label: "Twitter", className: "btn-info",
+                                        callback: function() {
+                                                window.open("https://twitter.com/search?q=%23E3", "_blank");
+                                                return false;
+                                        }
+                                },
+                                twitch: {
+                                        label: "Twitch", className: "btn-link",
+                                        callback: function() {
+                                                window.open("http://www.twitch.tv/event/e3", "_blank");
+                                                return false;
+                                        }
+                                },
+                                official: {
+                                        label: "Website", className: "btn-link",
+                                        callback: function() {
+                                                window.open("http://www.e3expo.com/", "_blank");
+                                                return false;
+                                        }
+                                },
+                                youtube: {
+                                        label: "YouTube", className: "btn-danger",
+                                        callback: function() {
+                                                window.open("https://www.youtube.com/results?search_query=E3", "_blank");
+                                                return false;
                                         }
                                 }
-                        });
-                };
+                        }
+                });
+        };
 });
 
-$(window).tn("tnLoad", secure + "//cdnjs.cloudflare.com/ajax/libs/store.js/1.3.14/store.min.js", function() {
+$(window).tn("load", "js", [secure + "//cdnjs.cloudflare.com/ajax/libs/store.js/1.3.14/store.min.js"], function() {
+        console.info("Store.js loaded.");
+        var form = document.querySelectorAll("#form");
+        var namespace = document.querySelectorAll("#namespace");
+        if (window.localStorage["_-Main"]) {
+                form.value = store.get("_-Main");
+        }
+});
+
+$(window).tn("load", "js", [secure + "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css"], function() {
         console.info("Store.js loaded.");
         var form = document.querySelectorAll("#form");
         var namespace = document.querySelectorAll("#namespace");
 });
 
-$(window).tn("tnLoad", secure + "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/moment.min.js", function() {
+$(window).tn("load", "js", [secure + "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/moment.min.js"], function() {
         console.log("Moment.JS Loaded");
         var UT = function() {
-                        // Making it work
-                        $("#form.day").attr("placeholder", moment(new Date()).format("[Hi, it's ]dddd[, the] Do [of] MMMM YYYY[ and time is] h:mm a[.]"));
-                        $("#form.night").attr("placeholder", moment(new Date()).format("[Hows it goin? it was a nice ]dddd, Do [of] MMMM YYYY[ and the time is] h:mm a[, goodnight.]"));
-                        // Pushing the Marquee, the hard and only way.
-                        if (0 <= new Date().getHours() && new Date().getHours() < 18) {
-                                $("#form").addClass("day");
-                        } else {
-                                $("#form").addClass("night");
-                        }
-                };
+                // Making it work
+                $("#form.day").attr("placeholder", moment(new Date()).format("[Hi, it's ]dddd[, the] Do [of] MMMM YYYY[ and time is] h:mm a[.]"));
+                $("#form.night").attr("placeholder", moment(new Date()).format("[Hows it goin? it was a nice ]dddd, Do [of] MMMM YYYY[ and the time is] h:mm a[, goodnight.]"));
+                // Pushing the Marquee, the hard and only way.
+                if (0 <= new Date().getHours() && new Date().getHours() < 18) {
+                        $("#form").addClass("day");
+                } else {
+                        $("#form").addClass("night");
+                }
+        };
         UT();
         setInterval(UT, 1);
 });
 
 $(function() {
-        $(window).tn("tnLoad", secure + "//cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js", function() {
+        $(window).tn("load", "js", [secure + "//cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"], function() {
                 console.info("Prefixfree Loaded");
         });
-        $(window).tn("tnLoad", [
-            secure + "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js",
-            secure + "//cdnjs.cloudflare.com/ajax/libs/bootstrap-growl/1.0.0/jquery.bootstrap-growl.min.js",
-            secure + "//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.2.0/bootbox.min.js"
+        $(window).tn("load", [
+                secure + "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js",
+                secure + "//cdnjs.cloudflare.com/ajax/libs/bootstrap-growl/1.0.0/jquery.bootstrap-growl.min.js",
+                secure + "//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.2.0/bootbox.min.js"
         ], function() {
                 console.info("Bootstrap, Bootstrap Growl and Bootbox has been loaded.");
         });
 });
 
-$(window).tn("tnLoad", secure + "//togetherjs.com/togetherjs-min.js", function() {
+$(window).tn("load", [secure + "//togetherjs.com/togetherjs-min.js"], function() {
         // Groupies
         TogetherJSConfig_siteName = "Textnet";
         TogetherJSConfig_toolName = "Groupies";
