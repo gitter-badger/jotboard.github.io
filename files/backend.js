01ea07d5c3db313596eb2106b826431dc511c230
Chris Lowles (Sociao) <sociao@users.noreply.github.com>
2014-06-26 16:13:31 +1200

    Update backend.js

--- files/backend.js
+++ files/backend.js
@@ -43,39 +43,39 @@ $(function() {
                         }
                 }
                 bootbox.dialog({
+                        title: "#WhatsCookin",
                         message: "<iframe class='block yt-player' width='100%' height='310' src='https://www.youtube.com/embed/?listType=playlist&list=PLXJjNJMpJQKqbpmhNOJNETiMbfZpLjIVb&fs=1&loop=1&showinfo=0&autohide=1&theme=light' frameborder='0' allowfullscreen></iframe>",
-                        title: "E3",
                         buttons: {
                                 facebook: {
-                                        label: "Facebook", className: "block btn-primary",
+                                        label: "Facebook", className: "btn-primary",
                                         callback: function() {
                                                 window.open("https://www.facebook.com/hashtag/E3", "_blank");
                                                 return false;
                                         }
                                 },
                                 twitter: {
-                                        label: "Twitter", className: "block btn-info",
+                                        label: "Twitter", className: "btn-info",
                                         callback: function() {
                                                 window.open("https://twitter.com/search?q=%23E3", "_blank");
                                                 return false;
                                         }
                                 },
                                 twitch: {
-                                        label: "Twitch", className: "block btn-link",
+                                        label: "Twitch", className: "btn-link",
                                         callback: function() {
                                                 window.open("http://www.twitch.tv/event/e3", "_blank");
                                                 return false;
                                         }
                                 },
                                 official: {
-                                        label: "Website", className: "block btn-link",
+                                        label: "Website", className: "btn-link",
                                         callback: function() {
                                                 window.open("http://www.e3expo.com/", "_blank");
                                                 return false;
                                         }
                                 },
                                 youtube: {
-                                        label: "YouTube", className: "block btn-danger",
+                                        label: "YouTube", className: "btn-danger",
                                         callback: function() {
                                                 window.open("https://www.youtube.com/results?search_query=E3", "_blank");
                                                 return false;
@@ -95,28 +95,22 @@ $(window).grab("js", [secure + "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0
                 }
                 $("#form.day").attr("placeholder", moment(new Date()).format("[Hi, it's ]dddd[, the] Do [of] MMMM YYYY[ and time is] h:mm a[.]"));
                 $("#form.night").attr("placeholder", moment(new Date()).format("[Hows it goin? it was a nice ]dddd, Do [of] MMMM YYYY[ and the time is] h:mm a[, goodnight.]"));
-                setInterval(UT, 1);
         };
-        window[UT()];
+        UT(); setInterval(UT, 1);
         console.info("MomentJS loaded");
 });
 
 $(window).grab("js", [secure + "//cdnjs.cloudflare.com/ajax/libs/store.js/1.3.14/store.min.js"], function() {
         var form = document.querySelectorAll("#form");
         var namespace = document.querySelectorAll("#namespace");
-        $(function() {
-                if (window.localStorage["_-Main"]) {
-                        form.value = store.get("_-Main");
-                }
-        });
+        if (window.localStorage["_-Main"]) {
+                form.value = store.get("_-Main");
+        }
         console.info("StoreJS loaded");
 });
 
 $(window).grab("js", [secure + "//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.2.0/bootbox.min.js"], function() {
         bootbox.setDefaults({
-                onEscape: function() {
-                        dialog.modal("hide");
-                },
                 backdrop: false,
                 animate: true
         });
@@ -128,9 +122,10 @@ $(window).grab("css", [secure + "//cdnjs.cloudflare.com/ajax/libs/twitter-bootst
 
 $(window).grab("js", [secure + "//cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"], function() {
         console.info("Prefixfree Loaded");
-        $(window).grab("css", [secure + "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css"], function() {
-                console.info("Font Awesome Loaded");
-        });
+});
+
+$(window).grab("css", [secure + "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css"], function() {
+        console.info("Font Awesome Loaded");
 });
 
 $(window).grab("js", [
@@ -138,14 +133,15 @@ $(window).grab("js", [
         secure + "//cdnjs.cloudflare.com/ajax/libs/bootstrap-growl/1.0.0/jquery.bootstrap-growl.min.js"
 ], function() {
         console.info("Bootstrap (JS) and Bootstrap Growl has been loaded.");
-        $(window).grab("js", [secure + "//togetherjs.com/togetherjs-min.js"], function() {
-                // Groupies
-                TogetherJSConfig_siteName = "Textnet";
-                TogetherJSConfig_toolName = "Groupies";
-                TogetherJSConfig_dontShowClicks = true;
-                TogetherJSConfig_youtube = true;
-                TogetherJSConfig_disableWebRTC = true;
-                TogetherJSConfig_suppressInvite = false;
-                TogetherJSConfig_suppressJoinConfirmation = true;
-        });
+});
+
+$(window).grab("js", [secure + "//togetherjs.com/togetherjs-min.js"], function() {
+        // Groupies
+        TogetherJSConfig_siteName = "Textnet";
+        TogetherJSConfig_toolName = "Groupies";
+        TogetherJSConfig_dontShowClicks = true;
+        TogetherJSConfig_youtube = true;
+        TogetherJSConfig_disableWebRTC = true;
+        TogetherJSConfig_suppressInvite = false;
+        TogetherJSConfig_suppressJoinConfirmation = true;
 });
