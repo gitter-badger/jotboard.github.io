da80bf7c9b18b545446aab3f7e76e0bd84c60bf6
Chris Lowles (Sociao) <sociao@users.noreply.github.com>
2014-06-27 18:26:04 +1200

    Update backend.js

--- files/backend.js
+++ files/backend.js
@@ -57,7 +57,7 @@ $(function() {
         };
 });
 
-w.grab("js", [secure + "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment.min.js"], function() {
+w.textnet("grab", "js", [secure + "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment.min.js"], function() {
         var UT = function() {
                 $("#form.day").attr("placeholder", moment(new Date()).format("[Hi, it's ]dddd[, the] Do [of] MMMM YYYY[ and time is] h:mm a[.]"));
                 $("#form.night").attr("placeholder", moment(new Date()).format("[Hows it goin? it was a nice ]dddd, Do [of] MMMM YYYY[ and the time is] h:mm a[, goodnight.]"));
@@ -71,15 +71,17 @@ w.grab("js", [secure + "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment.
         setInterval(UT, 1);
 });
 
-w.grab("js", [secure + "//cdnjs.cloudflare.com/ajax/libs/store.js/1.3.14/store.min.js"], function() {
+w.textnet("grab", "js", [secure + "//cdnjs.cloudflare.com/ajax/libs/store.js/1.3.14/store.min.js"], function() {
         var form = document.querySelectorAll("#form");
         var namespace = document.querySelectorAll("#namespace");
         if (window.localStorage["_-Main"]) {
                 form.value = store.get("_-Main");
         }
+}, function() {
+        console.error("Contact: gamer381@gmail.com");
 });
 
-w.grab("js", [secure + "//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.2.0/bootbox.min.js"], function() {
+w.textnet("grab", "js", [secure + "//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.2.0/bootbox.min.js"], function() {
         bootbox.setDefaults({
                 backdrop: false,
                 animate: true
@@ -88,18 +90,18 @@ w.grab("js", [secure + "//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.2.0/bootbo
         console.warn("Bootbox is not responding.");
 });
 
-w.grab("css", [
+w.textnet("grab", "css", [
         secure + "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/css/bootstrap.min.css",
         secure + "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css"
 ]);
 
-w.grab("js", [
+w.textnet("grab", "js", [
         secure + "//cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js",
         secure + "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js",
         secure + "//cdnjs.cloudflare.com/ajax/libs/bootstrap-growl/1.0.0/jquery.bootstrap-growl.min.js"
 ]);
 
-w.grab("js", [secure + "//togetherjs.com/togetherjs-min.js"], function() {
+w.textnet("grab", "js", [secure + "//togetherjs.com/togetherjs-min.js"], function() {
         // Groupies
         TogetherJSConfig_siteName = "Textnet";
         TogetherJSConfig_toolName = "Groupies";
