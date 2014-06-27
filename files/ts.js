315ed38d7a88ef885d5730f73a4821669958fb79
Chris Lowles (Sociao) <sociao@users.noreply.github.com>
2014-06-27 17:11:39 +1200

    Update ts.js

--- files/ts.js
+++ files/ts.js
@@ -2,6 +2,7 @@
 // Shrinking our need for adding the same sort of code.
 var secure = window.location.protocol;
 var hashline = function(u) { return window.location.href.indexOf("#" + u) != -1; };
+var createThis = function(u) { return document.createElement(u); };
 var grabSelector = function(u) { return document.querySelector(u); };
 var grabSelectorAll = function(u) { return document.querySelectorAll(u); };
 var w = $(window);
@@ -15,7 +16,7 @@ jQuery.fn.grab = function(syntax, url, success, fail) {
         var head = document.getElementsByTagName("head")[0];
         if (syntax == "js") {
                 url.forEach(function(src) {
-                        var script = document.createElement("script");
+                        var script = createThis("script");
                         script.type = "text/javascript";
                         script.src = src;
                         script.onload = script.onsuccess = function() {
@@ -41,7 +42,7 @@ jQuery.fn.grab = function(syntax, url, success, fail) {
         }
         if (syntax == "css") {
                 url.forEach(function(src) {
-                        var link = document.createElement("link");
+                        var link = createThis("link");
                         link.type = "text/css";
                         link.href = src;
                         link.onload = link.onsuccess = function() {
@@ -69,16 +70,20 @@ jQuery.fn.grab = function(syntax, url, success, fail) {
 
 // Universal Selector w.textnet
 jQuery.fn.textnet = function(tn, tn1, tn2, tn3) {
+        // w.grab (optional)
+        if (tn == "load") {
+                w.grab(tn, tn1, tn2, tn3);
+        }
         if (tn == "toggleAttr") {
                 return this.each(function() {
-                        if ($(this).attr(tn1) == tn2) $(this).attr(tn1, tn3);
-                        else $(this).attr(tn1, tn2);
+                        if (t.attr(tn1) == tn2) t.attr(tn1, tn3);
+                        else t.attr(tn1, tn2);
                 });
         }
         if (tn == "toggleHTML") {
                 return this.each(function() {
-                        if ($(this).html() == tn1) $(this).html(tn2);
-                        else $(this).html(tn1);
+                        if (t.html() == tn1) t.html(tn2);
+                        else t.html(tn1);
                 });
         }
         if (tn == "change") {
