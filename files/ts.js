8465f31b1db128b84a8c8479cac053035e53c0f6
Chris Lowles (Sociao) <sociao@users.noreply.github.com>
2014-06-29 02:51:02 +1200

    Update ts.js

--- files/ts.js
+++ files/ts.js
@@ -3,10 +3,9 @@
 var secure = window.location.protocol;
 var hashline = function(u) { return window.location.href.indexOf("#" + u) != -1; };
 var createThis = function(u) { return document.createElement(u); };
+var w = jQuery(window); var t = jQuery(this);
 var grabSelector = function(u) { return document.querySelector(u); };
 var grabSelectorAll = function(u) { return document.querySelectorAll(u); };
-var w = jQuery(window);
-var t = jQuery(this);
 Node.prototype.prependChild = function(el) {
         this.childNodes[1] && this.insertBefore(el, this.childNodes[1]) || this.appendChild(el);
 };
