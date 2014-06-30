be413c33fff6550bd90fb95d450a0d66b98ca4c8
Chris Lowles (Sociao) <sociao@users.noreply.github.com>
2014-07-01 01:05:25 +1200

    Update ts.js

--- files/ts.js
+++ files/ts.js
@@ -5,7 +5,7 @@ var hashline = function(u) { return window.location.href.indexOf("#" + u) != -1;
 var createThis = function(u) { return document.createElement(u); };
 var w = document.getElementsByTagName("body");
 var t = document.querySelectorAll(this) || document.querySelectorAll(this) || $(this);
-var t_TAttr = $(this);
+var jQThis = $(this);
 var grabSelector = function(u) { return document.querySelector(u); };
 var grabSelectorAll = function(u) { return document.querySelectorAll(u); };
 Node.prototype.prependChild = function(el) {
@@ -16,14 +16,14 @@ Node.prototype.prependChild = function(el) {
 jQuery.fn.textnet = function(tn, tn1, tn2, tn3, tn4) {
         if (tn == "toggleAttr") {
                 return this.each(function() {
-                        if (t_TAttr.attr(tn1) == tn2) t.attr(tn1, tn3);
-                        else t.attr(tn1, tn2);
+                        if (jQThis.attr(tn1) == tn2) jQThis.attr(tn1, tn3);
+                        else jQThis.attr(tn1, tn2);
                 });
         }
         if (tn == "toggleHTML") {
                 return this.each(function() {
-                        if (t.html() == tn1) t.html(tn2);
-                        else t.html(tn1);
+                        if (jQThis.html() == tn1) jQThis.html(tn2);
+                        else jQThis.html(tn1);
                 });
         }
         if (tn == "change") {
@@ -110,7 +110,7 @@ jQuery.fn.textnet = function(tn, tn1, tn2, tn3, tn4) {
                 }
                 if (tn1 == "groupies") {
                         TogetherJS(this);
-                        $.bootstrapGrowl("Don't give out personal information unless consent is given.", {
+                        $.bootstrapGrowl("Don't give out personal information unless official consent is given.", {
                                 ele: "body",
                                 type: "info",
                                 offset: {
