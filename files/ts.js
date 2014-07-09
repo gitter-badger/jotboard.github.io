4efcf7bb2e1a1685718850e1fa97f44dfb7fa522
Chris Lowles (Sociao) <sociao@users.noreply.github.com>
2014-07-10 01:47:50 +1200

    Update ts.js

--- files/ts.js
+++ files/ts.js
@@ -11,12 +11,12 @@ Node.prototype.prependChild = function(el) {
 // Universal Selector w.textnet
 jQuery.fn.textnet = function(tn, tn1, tn2, tn3, tn4) {
         if (tn == "toggleAttr") {
-                if (this.attr(tn1) == tn2) this.attr(tn1, tn3);
-                if (this.attr(tn1) == tn3) this.attr(tn1, tn2);
+                if (this.attr(tn1) === tn2) this.attr(tn1, tn3);
+                if (this.attr(tn1) === tn3) this.attr(tn1, tn2);
         }
         if (tn == "toggleHTML") {
-                if (this.innerHTML == tn1) this.innerHTML = tn2;
-                if (this.innerHTML == tn2) this.innerHTML = tn1;
+                if (this.innerHTML === tn1) this.innerHTML = tn2;
+                if (this.innerHTML === tn2) this.innerHTML = tn1;
         }
         if (tn == "change") {
                 if (tn1 == "save") {
