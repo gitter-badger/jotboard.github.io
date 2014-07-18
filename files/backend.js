42132dab1c20755f76455c234939b00882eacf86
Chris Lowles (Sociao) <sociao@users.noreply.github.com>
2014-07-19 02:21:39 +1200

    Update backend.js

--- files/backend.js
+++ files/backend.js
@@ -1,4 +1,4 @@
-var DevMode = true;
+var DevMode = false;
 head.load(["//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js", "files/ts.js"], function() {
         $(function() {
                 $("iframe").attr({
